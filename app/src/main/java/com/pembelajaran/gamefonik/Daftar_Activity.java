ebc14f8046a07718656999271efa49dd8753994c
bervinleonardy <bervin.leonardy@gmail.com>
2020-06-13 15:41:48 +0700

    Tambahan Controller Bitstream Upload Foto

--- app/src/main/java/com/pembelajaran/gamefonik/Daftar_Activity.java
+++ app/src/main/java/com/pembelajaran/gamefonik/Daftar_Activity.java
@@ -1,20 +1,32 @@
 package com.pembelajaran.gamefonik;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Calendar;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
@@ -32,23 +44,23 @@ public class Daftar_Activity extends AppCompatActivity {
     RadioButton jenis_kelamin;
     Button btnDaftar;
     Button upload_foto;
-    DatabaseHelper db;
+//    DatabaseHelper db;
+    ImageView imageView;
+
+
+    final int REQUEST_CODE_GALLERY = 999;
+
+    public static DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_2);
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        etConfirmPassword = findViewById(R.id.etConfirmPassword);
-        batal = findViewById(R.id.batal);
-        alamat = findViewById(R.id.alamat);
-        nama_lengkap = findViewById(R.id.namaLengkap);
-        btnDaftar = findViewById(R.id.btnDaftar);
-        tanggal_lahir=(EditText) findViewById(R.id.tanggal_lahir);
-        tanggal_lahir.setInputType(InputType.TYPE_NULL);
+        init();
+
         db = new DatabaseHelper(this);
-        radioJenis = (RadioGroup) findViewById(R.id.radioJenis);
+        db.queryData("CREATE TABLE IF NOT EXISTS t_user(ID INTEGER PRIMARY KEY, username TEXT, password TEXT, nama_lengkap TEXT, alamat TEXT, jenis_kel TEXT, tgl_lahir TEXT, foto_profil BLOB)");
+
         radioJenis.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
         {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -82,6 +94,17 @@ public class Daftar_Activity extends AppCompatActivity {
         });
         // tutup tanggal lahir
 
+        upload_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        Daftar_Activity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
+
 
         batal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,9 +124,10 @@ public class Daftar_Activity extends AppCompatActivity {
                 String almt = alamat.getText().toString().trim();
                 String jenis = jenis_kelamin.getText().toString().trim();
                 String tanggal = tanggal_lahir.getText().toString().trim();
-//                String upload = upload_foto.getText().toString().trim();
+                String upload = upload_foto.getText().toString().trim();
+                byte[] gambar = imageViewToByte(imageView);
                 if (pwd.equals(cnf_pwd)) {
-                    long val = db.addUser(user, pwd, nama, almt, jenis, tanggal);
+                    long val = db.addUser(user, pwd, nama, almt, jenis, tanggal, gambar);
                     if (val > 0) {
                         SweetAlertDialog pDialog = new SweetAlertDialog(Daftar_Activity.this, SweetAlertDialog.ERROR_TYPE);
                         pDialog.setTitleText("Peringatan!");
@@ -137,5 +161,69 @@ public class Daftar_Activity extends AppCompatActivity {
         });
     }
 
+    public static byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+            else {
+                SweetAlertDialog pDialog = new SweetAlertDialog(Daftar_Activity.this, SweetAlertDialog.ERROR_TYPE);
+                pDialog.setTitleText("Peringatan!");
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setContentText("Kamu tidak memiliki hak akses menuju lokasi data!");
+                pDialog.setCancelable(false);
+                pDialog.show();
+//                Toast.makeText(getApplicationContext(), "You don't have permission to access file location!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void init(){
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        batal = findViewById(R.id.batal);
+        alamat = findViewById(R.id.alamat);
+        nama_lengkap = findViewById(R.id.namaLengkap);
+        btnDaftar = findViewById(R.id.btnDaftar);
+        tanggal_lahir=(EditText) findViewById(R.id.tanggal_lahir);
+        tanggal_lahir.setInputType(InputType.TYPE_NULL);
+        radioJenis = (RadioGroup) findViewById(R.id.radioJenis);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        upload_foto = (Button) findViewById(R.id.upload_foto);
+    }
 
 }
