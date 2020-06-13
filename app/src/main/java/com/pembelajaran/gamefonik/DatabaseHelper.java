4ec7a398ed87f35c075e7afc9a744607ca8ea3c0
bervinleonardy <bervin.leonardy@gmail.com>
2020-06-13 15:44:13 +0700

    Tambahan Blob Sqlite

--- app/src/main/java/com/pembelajaran/gamefonik/DatabaseHelper.java
+++ app/src/main/java/com/pembelajaran/gamefonik/DatabaseHelper.java
@@ -3,12 +3,14 @@ package com.pembelajaran.gamefonik;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import android.text.TextUtils;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "gamefonik.db";
+    public static final String DATABASE_NAME = "gamefonik.sqlite";
     public static final String TABLE_NAME = "t_user";
     public static final String COL_ID = "ID";
     public static final String COL_USER = "username";
@@ -27,8 +29,15 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+ TABLE_NAME + "(ID TEXT PRIMARY KEY, username TEXT, password TEXT, nama_lengkap TEXT, alamat TEXT, jenis_kel TEXT, tgl_lahir TEXT, foto_profil BLOB)");
+        db.execSQL("CREATE TABLE "+ TABLE_NAME + "(ID INTEGER PRIMARY KEY , username TEXT, password TEXT, " +
+                "nama_lengkap TEXT, alamat TEXT, jenis_kel TEXT, tgl_lahir TEXT, " +
+                "foto_profil BLOB)");
         this.db = db;
+    }
+
+    public void queryData(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
     }
 
     @Override
@@ -38,7 +47,46 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         this.onCreate(db);
     }
 
-    public long addUser(String user, String password,String nama_lengkap, String alamat, String jenis_kel,String tgl_lahir){
+    public Cursor getData(String sql){
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql, null);
+    }
+
+    public  void deleteData(int ID) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "DELETE FROM t_user WHERE ID = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+        statement.bindDouble(1, (double)ID);
+
+        statement.execute();
+        database.close();
+    }
+    public void updateData(String user, String password,String nama_lengkap, String alamat,
+                           String jenis_kel,String tgl_lahir, byte[] foto_profil, int ID) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "UPDATE t_user SET user = ?, password = ?, nama_lengkap = ?,alamat= ? ," +
+                "jenis_kel = ?, tgl_lahir = ?, foto_profil = ? WHERE ID = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+
+        statement.bindString(1, user);
+        statement.bindString(2, password);
+        statement.bindString(3,nama_lengkap);
+        statement.bindString(4,alamat);
+        statement.bindString(5,jenis_kel);
+        statement.bindString(6,tgl_lahir);
+        statement.bindBlob(7, foto_profil);
+        statement.bindDouble(8, (double)ID);
+
+        statement.execute();
+        database.close();
+    }
+
+    public long addUser(String user, String password,String nama_lengkap, String alamat,
+                        String jenis_kel,String tgl_lahir, byte[] foto_profil) throws SQLException
+    {
         db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         //create a string query
@@ -51,7 +99,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         contentValues.put("alamat",alamat);
         contentValues.put("jenis_kel",jenis_kel);
         contentValues.put("tgl_lahir",tgl_lahir);
-//        contentValues.put("foto_profil",foto_profil);
+        contentValues.put("foto_profil",foto_profil);
         long res = db.insert("t_user",null, contentValues);
         db.close();
         return res;
