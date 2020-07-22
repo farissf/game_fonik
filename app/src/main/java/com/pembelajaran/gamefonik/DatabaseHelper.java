c1d5dc84032f5e99714ebc46dd6107cc5f001b34
Muhammad Faris <faristctclub@gmail.com>
2020-07-22 13:04:46 +0700

    Menambah soal

--- app/src/main/java/com/pembelajaran/gamefonik/DatabaseHelper.java
+++ app/src/main/java/com/pembelajaran/gamefonik/DatabaseHelper.java
@@ -27,12 +27,19 @@ public class DatabaseHelper extends SQLiteOpenHelper{
        String t_user = "CREATE TABLE "+TABLE1+"(ID INTEGER PRIMARY KEY , username TEXT, password TEXT, " +
                 "nama_lengkap TEXT, alamat TEXT, jenis_kel TEXT, tgl_lahir TEXT, " +
                 "foto_profil BLOB)";
-        String t_soal = "CREATE TABLE "+TABLE2+"(id_soal INTEGER PRIMARY KEY , img_soal BLOB, img_utama BLOB, " +
-                "kata TEXT)";
-
-
         db.execSQL(t_user);
+        String t_soal = "CREATE TABLE "+TABLE2+"(id_soal INTEGER PRIMARY KEY AUTOINCREMENT,kata String)";
         db.execSQL(t_soal);
+        t_soal = "INSERT INTO t_soal (id_soal,kata) VALUES (1,'a'),(2,'b'),(3,'c'),(4,'d'),(5,'e'),(6,'f'),(7,'g'),(8,'h')" +
+                ",(9,'i'),(10,'j'),(11,'k'),(12,'l'),(13,'m'),(14,'n'),(15,'o'),(16,'p'),(17,'q'),(18,'r'),(19,'s'),(20,'t'),(21,'u'),(22,'v')" +
+                ",(23,'w'),(24,'x'),(25,'y'),(26,'z'),(27,'ba'),(28,'bi'),(29,'bu'),(30,'be'),(31,'bo'),(32,'ca'),(33,'ci'),(34,'cu'),(35,'ce'),(36,'co')" +
+                ",(37,'da'),(38,'di'),(39,'du'),(30,'de'),(31,'do'),(32,'fa'),(33,'fi'),(34,'fu'),(35,'fe'),(36,'fo'),(37,'ga'),(38,'gi'),(39,'gu'),(40,'ge'),(32,'go')," +
+                "(33,'ha'),(34,'hi'),(35,'hu'),(36,'he'),(37,'ho'),(38,'ja'),(39,'ji'),(40,'ju'),(41,'je'),(42,'jo'),(43,'ka'),(44,'ki'),(45,'ku'),(46,'ke'),(47,'ko')," +
+                "(48,'la'),(49,'li'),(50,'lu'),(30,'le'),(31,'lo'),(32,'ma'),(33,'mi'),(34,'mu'),(35,'me'),(36,'mo'),(37,'na'),(38,'ni'),(39,'nu'),(40,'ne'),(41,'no')," +
+                "(42,'pa'),(43,'pi'),(44,'pu'),(45,'pe'),(46,'po'),(47,'ra'),(48,'ri'),(49,'ru'),(50,'re'),(51,'ro'),(52,'sa'),(53,'si'),(54,'su'),(55,'se'),(56,'so')," +
+                "(57,'ta'),(58,'ti'),(59,'tu'),(60,'te'),(61,'to'),(62,'za'),(63,'zi'),(64,'zu'),(65,'ze'),(66,'zo')";
+        db.execSQL(t_soal);
+
 
 
     }
@@ -48,6 +55,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         db.execSQL("DROP TABLE IF EXISTS "+TABLE2);
         db.execSQL("DROP TABLE IF EXISTS "+TABLE3);
         onCreate(db);
+    }
+
+    public void insertData(String kata){
+
+
     }
 
     public Cursor LihatData() {
